5103bc95e7155bb6733f84c81add6b254d8aaf7e
wobsoriano <sorianorobertc@gmail.com>
2022-11-11 12:08:44 -0800

    update api/ssr

--- api/ssr.js
+++ api/ssr.js
@@ -1,5 +1,7 @@
 import { renderPage } from 'vite-plugin-ssr'
 
+import '../dist/server/importBuild.js'
+
 export default async function handler (req, res) {
   const { url } = req
   console.log('Request to url:', url)
